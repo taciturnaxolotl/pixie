287948a66eb25dda7324766ca886ee81678a1590
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-03-23 19:34:16 -0400

    feat: added slack auth and handling

--- netlify/functions/magic-dust/index.ts
+++ netlify/functions/magic-dust/index.ts
@@ -1,18 +1,65 @@
 import type { Context } from "@netlify/functions";
 import Airtable from "airtable";
+import { jwtDecode } from "jwt-decode";
 
 export default async (req: Request, context: Context) => {
-    if (req.method !== 'GET') {
-        return new Response('Method Not Allowed', { status: 405 });
-    } else if (!req.url.includes('?')) {
-        return new Response('Bad Request', { status: 400 });
-    } else if (!req.url.includes('portal=')) {
-        return new Response('Missing portal parameter', { status: 400 });
+    // get the portal from the magicKey cookie
+    const magicKey = context.cookies.get("magicKey");
+    context.cookies.delete("magicKey");
+
+    // get profile information from the slack callback
+    const code = new URLSearchParams(req.url.split("?")[1]).get("code");
+
+    if (code === null) {
+        return new Response("Invalid code", { status: 400 });
     }
 
-    const portal = req.url.split('portal=')[1];
+    console.log(code);
 
-    interface Record {
+    // exchange the code for an access token
+    const slackToken = await (await fetch("https://slack.com/api/openid.connect.token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            code: code,
+            client_id: process.env.SLACK_CLIENT_ID as string,
+            client_secret: process.env.SLACK_CLIENT_SECRET as string,
+            redirect_uri: "https://localhost:8888/.netlify/functions/magic-dust"
+        })
+    })).json();
+
+    if (!slackToken.ok) {
+        return new Response(`Failed to get token: ${slackToken.error}`, { status: 500 });
+    }
+
+    type SlackProfile = {
+        iss: string,
+        sub: string
+        aud: string,
+        exp: number,
+        iat: number,
+        auth_time: number,
+        nonce: string,
+        at_hash: string,
+        'https://slack.com/team_id': string,
+        'https://slack.com/user_id': string,
+        locale: string,
+        name: string,
+        picture: string,
+        given_name: string,
+        family_name: string,
+        'https://slack.com/team_name': string,
+        'https://slack.com/team_domain': string,
+        'https://slack.com/team_image_230': string,
+        'https://slack.com/team_image_default': boolean
+    }
+
+    // decode the id_token as a JWT
+    const profile: SlackProfile = jwtDecode(slackToken.id_token);
+
+    interface Portal {
         "Loot Count": number;
         "Name": string;
         "Items": string[];
@@ -21,8 +68,8 @@ export default async (req: Request, context: Context) => {
         "Loot Amount": number;
     }
 
-    function convertToRecord(obj: any): Record[] {
-        let records: Record[] = [];
+    function convertToRecord(obj: any): Portal[] {
+        let records: Portal[] = [];
 
         obj.forEach((record: any) => {
             records.push({
@@ -47,17 +94,16 @@ export default async (req: Request, context: Context) => {
         view: "Grid view"
     }).all());
 
+    const portal: Portal | undefined = portals.find((record) => record.Name === magicKey);
     // if name = portal, then return the portal
-    if (portals.find((record) => record.Name === portal) === undefined) {
-        return new Response('Portal not found', { status: 404 });
+    if (portal === undefined) {
+        return new Response(`Portal not found: ${portal}`, { status: 404 });
     }
 
-    const result = portals.find((record) => record.Name === portal);
-
-    const itemsString: string = (result as Record).Items.map((item, index) => {
-        if (index === (result as Record).Items.length - 1) {
+    const itemsString: string = (portal).Items.map((item, index) => {
+        if (index === (portal).Items.length - 1) {
             return item.replace(/^:-/, "").replace(/:$/, "");
-        } else if (index === (result as Record).Items.length - 2) {
+        } else if (index === (portal).Items.length - 2) {
             return item.replace(/^:-/, "").replace(/:$/, "") + " and";
         } else {
             return item.replace(/^:-/, "").replace(/:$/, "") + ",";
@@ -67,7 +113,7 @@ export default async (req: Request, context: Context) => {
     const response = {
         statusCode: 302,
         headers: {
-            Location: "http://localhost:8888/m/?portal=" + portal + "&items=" + itemsString,
+            Location: "http://localhost:8888/m/?portal=" + encodeURIComponent(magicKey) + "&count=" + encodeURIComponent(portal["Loot Amount"]) + "&items=" + encodeURIComponent(itemsString) + "&name=" + encodeURIComponent(profile.given_name),
             'Cache-Control': 'no-cache' // Disable caching of this response
         },
         body: '' // return body for local dev
