026cb59f5eb2c88ae555894026011a3239faf25b
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-03-24 12:47:42 -0400

    feat: log replacing string and value

--- plugins/replace/index.js
+++ plugins/replace/index.js
@@ -3,7 +3,11 @@ import path from "path";
 
 export const onPreBuild = function ({ inputs }) {
 	if (process.env.NETLIFY) {
-		console.log("Running replace plugin");
+		console.log(
+			`Running replace plugin: ${inputs.replace_string} -> ${
+				process.env[inputs.env_replace_with]
+			}`
+		);
 		// Replace the string in the file
 		const filePath = path.resolve(inputs.replace_path);
 		const fileContent = fs.readFileSync(filePath, "utf8");
